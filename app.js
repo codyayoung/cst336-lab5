3e1284e0e1c2b6940d6d1df605da37886bb8fe7a
Cody Young <cyoung1623@gmail.com>
2019-07-28 22:57:08 -0700

    3x3 grid of random images working

--- app.js
+++ app.js
@@ -11,18 +11,41 @@ const mysql = require("mysql");
 
 //Root
 app.get("/", function(req, res){
-
     var requestURL = "https://api.unsplash.com/photos/random?client_id=69c6610d92b1426c8a6a99aac12a2e723148b958d0f53d4bc743e68bd611cf74&orientation=landscape";
-
     request(requestURL, function(error, response, body){
 
         if (!error) {
             var parsedData = JSON.parse(body);
-            var imageURL = parsedData["urls"]["regular"];
+            var imageURL = parsedData.urls.regular;
             res.render("index", {"imageURL":imageURL});
         }
         else {
             res.render("index", {"error": "Unable to access Unsplash API."});
+        }
+    });
+
+});
+
+//Search
+app.get("/search", function(req, res){
+    //console.log(req.query.keyword);
+    var keyword = req.query.keyword;
+    var requestURL = "https://api.unsplash.com/photos/random?query="+keyword+"&count=9&client_id=69c6610d92b1426c8a6a99aac12a2e723148b958d0f53d4bc743e68bd611cf74&orientation=landscape";
+    request(requestURL, function(error, response, body){
+
+        if (!error) {
+            var parsedData = JSON.parse(body);
+            
+            //Initialize array to hold random image URLs, populate array
+            var imageURLs = [];
+            for(let i=0; i<9; i++){
+                imageURLs.push(parsedData[i].urls.regular);
+            }
+       
+            res.render("results", {"imageURLs":imageURLs});
+        }
+        else {
+            res.render("results", {"error": "Unable to access Unsplash API."});
         }
     });
 
